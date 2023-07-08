535898dab2544b76566f25411d5a8c2eb658c18d
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-07-08 12:31:09 +0300

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -5,20 +5,17 @@ import path from "path";
 const contactsPath = path.resolve("db", "contacts.json");
 
 export const listContacts = async () => {
-	// ...твій код. Повертає масив контактів.
 	const data = await fs.readFile(contactsPath);
 	return JSON.parse(data);
 };
 
 export const getContactById = async contactId => {
-	// ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
 	const contacts = await listContacts();
 	const result = contacts.find(contact => contact.id === contactId);
 	return result || null;
 };
 
 export const addContact = async ({ name, email, phone }) => {
-	// ...твій код. Повертає об'єкт доданого контакту.
 	const contacts = await listContacts();
 	const newContact = {
 		id: nanoid(),
@@ -32,7 +29,6 @@ export const addContact = async ({ name, email, phone }) => {
 };
 
 export const removeContact = async contactId => {
-	// ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
 	const contacts = await listContacts();
 	const index = contacts.findIndex(item => item.id === contactId);
 	if (index === -1) {
